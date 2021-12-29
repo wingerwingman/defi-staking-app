87171028fc4576f032ac1d71eb2f4e43a47fa8a2
Justin Winger <justinwinger4@gmail.com>
2021-12-29 12:15:06 -0700

    setup deployment

--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,13 @@ require('babel-polyfill');
 module.exports = {
     networks:{
         development: {
-            host: '127.0.0.1:7545',
+            host: '172.18.48.1',
             port: '7545',
             network_id: '*'
         },
     },
     contracts_directory: './src/contracts/',
-    contracts_build_directory: '/src/truffle_abis/',
+    contracts_build_directory: './src/truffle_abis/',
     compilers: {
         solc: {
             version: '^0.5.0',
