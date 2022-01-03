64b12b3ed13184156254f82feade2b4234cd6aa4
Justin Winger <justinwinger4@gmail.com>
2022-01-03 12:45:24 -0700

    setup coin transfers

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,14 +3,22 @@ const ClimbCoin = artifacts.require('ClimbCoin');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Tether)
+    const tether = await Tether.deployed()
 
     await deployer.deploy(ClimbCoin)
+    const climbcoin = await ClimbCoin.deployed()
 
     await deployer.deploy(RWD)
+    const rwd = await RWD.deployed()
 
     await deployer.deploy(DecentralBank)
-    
+    const decentralBank = await DecentralBank.deployed()
+
+    await rwd.transfer(DecentralBank.address, '1000000000000000000000000')
+
+    await tether.transfer(accounts[1], '1000000000000000000')
+
 };
 
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.5.0;
+import './RWD';
+import './Tether';
+import './ClimbCoin';
 
 contract DecentralBank {
     string public name = 'Decentral Bank';
     address public owner;
+    Tether public tether;
+    RWD public rwd;
+    ClimbCoin public climbcoin;
     
+    constructor(RWD _rwd, Tether _tether, CimbCoin _climbcoin) public {
+        rwd = _rwd;
+        tether = _tether;
+        climbcoin = _climbcoin;
+    }
 }
