b87e244f425f325c47ff7852e3adf043cb151e1e
Justin Winger <justinwinger4@gmail.com>
2022-01-06 12:13:28 -0700

    added navbar

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import './App.css';
+import Navbar from './Navbar';
+
+class App extends Component {
+
+    render () {
+        return (
+            <div>
+                <Navbar/>
+                <div className="text-center">
+                    Hello World
+                </div>
+            </div>
+        )
+    }
+}
+
+export default (App);
