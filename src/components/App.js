c40624de8a0fb352164d892ccaf02c5b0f0e751b
Justin Winger <justinwinger4@gmail.com>
2022-01-10 12:31:51 -0700

    setup web3

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,43 @@
 import React, {Component} from "react";
 import './App.css';
 import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        if(window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+            } else if(window.web3) {
+                    window.web3 = new Web3(window.web3.currentProvider)
+            } else {
+                    window.alert('No ethereum browser detected! You can check out MetaMask!')
+            }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const accounts = await web3.eth.getAccounts()
+        console.log(accounts)
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '0x0'
+        }
+    }
 
     render () {
         return (
             <div>
-                <Navbar/>
-                <div className="text-center">
-                    Hello World
-                </div>
+                <Navbar account={this.state.account} />
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ class Navbar extends Component {
         return (
             <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: "black", height: '50px'}}>
                 <a className='navbar-brand col-sm-3 col-md-2 mr-0'
-                style={{color:"white"}}><img src={bank} width='50' height='30' className='d-inline-block align-top' alt='Bank Image' />
+                style={{color:"white"}}><img src={bank} width='50' height='30' className='d-inline-block align-top' alt='Bank' />
                 &nbsp; DAPP Yield Staking (Decentralized Banking) 
                 </a>
                 <ul className='navbar-nav px-3'>
                     <li className='text-nowrap d-none nav-item d-sm-none d-sm-block'>
-                        <small style={{color:'white'}}>ACCOUNT NUMBER:
+                        <small style={{color:'white'}}>ACCOUNT NUMBER: {this.props.account}
                         </small>
                     </li>
                 </ul>
